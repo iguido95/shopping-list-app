bbdafc0329da6271ec76688699d882bd3adf8342
Guido Teunissen <iguido@me.com>
2016-04-05 19:21:30 +0200

    Sendgrid implementation for Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Mailer
+  # Mailer Settings
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000'}
 
